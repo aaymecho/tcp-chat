90e65389bec989c9f52abf7b29af8581e2cecbe4
Slim <aaymecho3@gatech.edu>
2023-11-21 19:05:45 -0500

    added closing features for server and client + header comments

--- client.cpp
+++ client.cpp
@@ -1,3 +1,16 @@
+/*
+Author: your name
+Class: ECE4122 or ECE6122 (A)
+Last Date Modified: 11/21/2023
+Description:
+This file acts as a TCP client, it continously asks the end-user to input a command:
+    1. v [nVersiion] changes the version number (ignores every message until v = 102)
+    2. t [nType]
+        a) if nType is 77 it sends to all clients except the sender.
+        b) if nType is 201 it sends the reverse message to the sender.
+        c) exit just closes connection
+    3. q command closes connection to server for client.
+*/
 #include <arpa/inet.h>
 #include <cstring>
 #include <iostream>
@@ -8,91 +21,115 @@
 #include <unistd.h>
 
 // Structure for TCP messages
-struct tcpMessage {
-  unsigned char
-      nVersion;        // Version of the protocol (accepts 102 ignores others)
-  unsigned char nType; // Type of the message (77 normal or 201 reverse)
-  unsigned short nMsgLen; // Length of the message
-  char chMsg[1000];       // Message content
+struct tcpMessage
+{
+    unsigned char nVersion; // Version of the protocol (accepts 102 ignores others)
+    unsigned char nType;    // Type of the message (77 normal or 201 reverse)
+    unsigned short nMsgLen; // Length of the message
+    char chMsg[1000];       // Message content
 };
 
 // Function to continuously receive messages from server
-void receive_messages(int server_socket) {
-  tcpMessage msg;
-  while (true) {
-    std::memset(&msg, 0, sizeof(msg));
-    ssize_t bytes_received = recv(server_socket, &msg, sizeof(msg), 0);
-    if (bytes_received <= 0) {
-      std::cerr << "Connection closed or error occurred." << std::endl;
-      break;
+void receiveMessages(int serverSocket)
+{
+    tcpMessage msg;
+    while (true)
+    {
+        std::memset(&msg, 0, sizeof(msg));
+        ssize_t bytesReceived = recv(serverSocket, &msg, sizeof(msg), 0);
+        if (bytesReceived <= 0)
+        {
+            std::cerr << "Connection closed." << std::endl;
+            break;
+        }
+        std::cout << "Received Msg Type: " << static_cast<int>(msg.nType)
+                  << "; Msg: " << msg.chMsg << std::endl;
     }
-    std::cout << "Received Msg Type: " << static_cast<int>(msg.nType)
-              << "; Msg: " << msg.chMsg << std::endl;
-  }
 }
 
-int main(int argc, char *argv[]) {
-  if (argc != 3) {
-    std::cerr << "Usage: " << argv[0] << " <IP Address> <Port>" << std::endl;
-    return 1;
-  }
-
-  std::string server_ip = argv[1];
-  int port = std::stoi(argv[2]);
-  int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_socket < 0) {
-    std::cerr << "Cannot open socket" << std::endl;
-    return 1;
-  }
-
-  sockaddr_in server_addr{};
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = htons(port);
-
-  if (inet_pton(AF_INET, server_ip.c_str(), &server_addr.sin_addr) <= 0) {
-    std::cerr << "Invalid address/ Address not supported" << std::endl;
-    return 1;
-  }
-
-  if (connect(server_socket, (struct sockaddr *)&server_addr,
-              sizeof(server_addr)) < 0) {
-    std::cerr << "Connection Failed" << std::endl;
-    return 1;
-  }
-
-  std::cout << "Connected to the server at " << server_ip << ":" << port
-            << std::endl;
-
-  std::thread receive_thread(receive_messages, server_socket);
-
-  std::string input;
-  tcpMessage msg;
-  while (true) {
-    std::cout << "Please enter command: ";
-    std::getline(std::cin, input);
-
-    if (input.empty())
-      continue;
-
-    if (input[0] == 'q') {
-      break;
-    } else if (input[0] == 'v') {
-      msg.nVersion = static_cast<unsigned char>(std::stoi(input.substr(2)));
-    } else if (input[0] == 't') {
-      size_t space_pos = input.find(' ', 2);
-      msg.nType =
-          static_cast<unsigned char>(std::stoi(input.substr(2, space_pos - 2)));
-      std::string message_content = input.substr(space_pos + 1);
-      msg.nMsgLen = static_cast<unsigned short>(message_content.size());
-      std::strncpy(msg.chMsg, message_content.c_str(), sizeof(msg.chMsg));
-      msg.chMsg[sizeof(msg.chMsg) - 1] = '\0';
-
-      send(server_socket, &msg, sizeof(msg), 0);
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " <IP Address> <Port>" << std::endl;
+        return 1;
     }
-  }
 
-  close(server_socket);
-  receive_thread.join();
+    std::string serverIp = argv[1];
+    int port = std::stoi(argv[2]);
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    // Check if input is 'localhost' and convert it to '127.0.0.1'
+    if (serverIp == "localhost")
+    {
+        serverIp = "127.0.0.1";
+    }
 
-  return 0;
+    if (serverSocket < 0)
+    {
+        std::cerr << "Cannot open socket" << std::endl;
+        return 1;
+    }
+
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(port);
+
+    if (inet_pton(AF_INET, serverIp.c_str(), &serverAddr.sin_addr) <= 0)
+    {
+        std::cerr << "Invalid address/ Address not supported" << std::endl;
+        return 1;
+    }
+
+    if (connect(serverSocket, (struct sockaddr *)&serverAddr,
+                sizeof(serverAddr)) < 0)
+    {
+        std::cerr << "Connection Failed" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Connected to the server at " << serverIp << ":" << port
+              << std::endl;
+
+    std::thread receiveThread(receiveMessages, serverSocket);
+
+    std::string input;
+    tcpMessage msg;
+    while (true)
+    {
+        std::cout << "Please enter command: ";
+        std::getline(std::cin, input);
+
+        if (input.empty())
+            continue;
+
+        if (input[0] == 'q')
+        {
+            // Send a disconnect message to the server
+            msg.nType = 255;
+            msg.nMsgLen = 0;
+            send(serverSocket, &msg, sizeof(msg), 0);
+            break;
+        }
+        else if (input[0] == 'v')
+        {
+            msg.nVersion = static_cast<unsigned char>(std::stoi(input.substr(2)));
+        }
+        else if (input[0] == 't')
+        {
+            size_t spacePos = input.find(' ', 2);
+            msg.nType =
+                static_cast<unsigned char>(std::stoi(input.substr(2, spacePos - 2)));
+            std::string messageContent = input.substr(spacePos + 1);
+            msg.nMsgLen = static_cast<unsigned short>(messageContent.size());
+            std::strncpy(msg.chMsg, messageContent.c_str(), sizeof(msg.chMsg));
+            msg.chMsg[sizeof(msg.chMsg) - 1] = '\0';
+
+            send(serverSocket, &msg, sizeof(msg), 0);
+        }
+    }
+
+    close(serverSocket);
+    if (receiveThread.joinable())
+        receiveThread.join();
+    return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,3 +1,18 @@
+/*
+Author: your name
+Class: ECE4122 or ECE6122 (A)
+Last Date Modified: 11/21/2023
+Description:
+This file acts as a TCP server which itcontinuously asks for user command while handling multiple
+client connections, making it so that it recieves/sending messages:
+    1. Sends all clients except the sender the message if nType is 77
+    2. Sends sender reverse message if nType is 201.
+    3. msg (prints last message saved from client)
+    4. exit closes connection of server and clients
+One thing to note is that the server will ignore any messages until the client has
+set the nVersion (version protocol) to 102 and remains at 102. One additional feature, is that
+it also lists the clients connection and +their information (used by typing clients in server command).
+*/
 #include <algorithm>
 #include <arpa/inet.h>
 #include <cstring>
@@ -9,175 +24,242 @@
 #include <thread>
 #include <unistd.h>
 #include <vector>
+#include <atomic>
+#include <signal.h>
 
 /*----------------------------------Function-Definitions-Start----------------------------------*/
 // Structure for TCP messages
-struct tcpMessage {
-  unsigned char
-      nVersion;        // Version of the protocol (accepts 102 ignores others)
-  unsigned char nType; // Type of the message (77 normal or 201 reverse)
-  unsigned short nMsgLen; // Length of the message
-  char chMsg[1000];       // Message content
+struct tcpMessage
+{
+    unsigned char nVersion; // Version of the protocol (accepts 102 ignores others)
+    unsigned char nType;    // Type of the message (77 normal or 201 reverse)
+    unsigned short nMsgLen; // Length of the message
+    char chMsg[1000];       // Message content
 };
 
 // Global variables
 std::map<int, sockaddr_in> clients;
-std::mutex clients_mutex;
+std::mutex clientsMutex;
 tcpMessage lastReceivedMessage;
 bool messageAvailable = false;
+std::atomic<bool> serverRunning(true); // Manage server state with safety (regular bool has restrictions)
 
 // Function to reverse the message content
-void reverse_message(tcpMessage &msg) {
-  std::reverse(msg.chMsg, msg.chMsg + msg.nMsgLen);
+void reverseMessage(tcpMessage &msg)
+{
+    std::reverse(msg.chMsg, msg.chMsg + msg.nMsgLen);
 }
 
 // Function to send a message to all clients except the sender
-void send_message_to_all_clients(int sender_socket, const tcpMessage &msg) {
-  std::lock_guard<std::mutex> lock(clients_mutex);
-  for (const auto &client : clients) {
-    if (client.first != sender_socket) {
-      send(client.first, &msg, sizeof(msg), 0);
+void sendMessageToAllClients(int senderSocket, const tcpMessage &msg)
+{
+    std::lock_guard<std::mutex> lock(clientsMutex);
+    for (const auto &client : clients)
+    {
+        if (client.first != senderSocket)
+        {
+            send(client.first, &msg, sizeof(msg), 0);
+        }
     }
-  }
 }
 
 // Function to handle individual client connections
-void handle_client(int client_socket) {
-  sockaddr_in client_address;
-  {
-    std::lock_guard<std::mutex> lock(clients_mutex);
-    client_address = clients[client_socket];
-  }
-
-  while (true) {
-    tcpMessage msg;
-    ssize_t bytes_received = recv(client_socket, &msg, sizeof(msg), 0);
-    if (bytes_received <= 0)
-      break;
-
-    // Update the last message received
+void handleClient(int clientSocket)
+{
+    sockaddr_in clientAddress;
     {
-      std::lock_guard<std::mutex> lock(clients_mutex);
-      lastReceivedMessage = msg;
-      messageAvailable = true;
+        std::lock_guard<std::mutex> lock(clientsMutex);
+        clientAddress = clients[clientSocket];
     }
 
-    if (msg.nVersion == 102) {
-      if (msg.nType == 77) {
-        send_message_to_all_clients(client_socket, msg);
-      } else if (msg.nType == 201) {
-        reverse_message(msg);
-        send(client_socket, &msg, sizeof(msg), 0);
-      }
-    }
-  }
+    while (true)
+    {
+        tcpMessage msg;
+        ssize_t bytesReceived = recv(clientSocket, &msg, sizeof(msg), 0);
+        if (bytesReceived <= 0)
+            break;
 
-  close(client_socket);
-  {
-    std::lock_guard<std::mutex> lock(clients_mutex);
-    clients.erase(client_socket);
-    std::cout << "Client disconnected: " << inet_ntoa(client_address.sin_addr)
-              << ":" << ntohs(client_address.sin_port) << std::endl;
-  }
+        // Detect disconnection message
+        if (msg.nType == 255)
+        {
+            std::cout << "Client requested disconnection: " << inet_ntoa(clientAddress.sin_addr)
+                      << ":" << ntohs(clientAddress.sin_port) << std::endl;
+            break; // Break out of the loop to close the socket
+        }
+
+        if (msg.nVersion == 102)
+        {
+            if (msg.nType == 77)
+            {
+                sendMessageToAllClients(clientSocket, msg);
+
+                // Update the last message received
+                {
+                    std::lock_guard<std::mutex> lock(clientsMutex);
+                    lastReceivedMessage = msg;
+                    messageAvailable = true;
+                }
+            }
+            else if (msg.nType == 201)
+            {
+                reverseMessage(msg);
+                send(clientSocket, &msg, sizeof(msg), 0);
+
+                // Update the last message received
+                {
+                    std::lock_guard<std::mutex> lock(clientsMutex);
+                    lastReceivedMessage = msg;
+                    messageAvailable = true;
+                }
+            }
+        }
+        else
+        {
+            continue;
+        }
+    }
+
+    close(clientSocket);
+    {
+        std::lock_guard<std::mutex> lock(clientsMutex);
+        clients.erase(clientSocket);
+        std::cout << "Client disconnected: " << inet_ntoa(clientAddress.sin_addr)
+                  << ":" << ntohs(clientAddress.sin_port) << std::endl;
+    }
 }
 
-// Function for the main command loop (continously asks for end-user to input
-// command)
-void command_loop() {
-  std::string command;
-  while (true) {
-    std::cout << "Please enter command: ";
-    std::cin >> command;
-    if (command == "msg") {
-      std::lock_guard<std::mutex> lock(clients_mutex);
-      if (messageAvailable) {
-        std::cout << "Last Message: " << lastReceivedMessage.chMsg << std::endl;
-      } else {
-        std::cout << "No messages received yet." << std::endl;
-      }
-    } else if (command == "clients") {
-      std::lock_guard<std::mutex> lock(clients_mutex);
-      std::cout << "Number of Clients: " << clients.size() << std::endl;
-      for (const auto &client : clients) {
-        std::cout << "IP Address: " << inet_ntoa(client.second.sin_addr)
-                  << " | Port: " << ntohs(client.second.sin_port) << std::endl;
-      }
-    } else if (command == "exit") {
-      break;
-    } else {
-      std::cout << "Unknown command." << std::endl;
+// Function for the main command loop (continuously asks for end-user to input command)
+void commandLoop()
+{
+    std::string command;
+    while (true)
+    {
+        std::cout << "Please enter command: ";
+        std::cin >> command;
+        if (command == "msg")
+        {
+            std::lock_guard<std::mutex> lock(clientsMutex);
+            if (messageAvailable)
+            {
+                std::cout << "Last Message: " << lastReceivedMessage.chMsg << std::endl;
+            }
+            else
+            {
+                std::cout << "No messages received yet." << std::endl;
+            }
+        }
+        else if (command == "clients")
+        {
+            std::lock_guard<std::mutex> lock(clientsMutex);
+            std::cout << "Number of Clients: " << clients.size() << std::endl;
+            for (const auto &client : clients)
+            {
+                std::cout << "IP Address: " << inet_ntoa(client.second.sin_addr)
+                          << " | Port: " << ntohs(client.second.sin_port) << std::endl;
+            }
+        }
+        else if (command == "exit")
+        {
+            // Notify clients of server shutdown
+            tcpMessage shutdownMsg;
+            shutdownMsg.nVersion = 102;
+            shutdownMsg.nType = 77; // Normal message type
+            strcpy(shutdownMsg.chMsg, "Server is shutting down.");
+            shutdownMsg.nMsgLen = strlen(shutdownMsg.chMsg);
+            sendMessageToAllClients(-1, shutdownMsg); // -1 indicates sending to all clients
+
+            serverRunning = false; // Set the server running flag to false
+            break;
+        }
+        else
+        {
+            std::cout << "Unknown command." << std::endl;
+        }
     }
-  }
 }
 
 // Function to accept incoming client connections
-void accept_connections(int server_socket) {
-  while (true) {
-    sockaddr_in client_address;
-    socklen_t client_address_len = sizeof(client_address);
-    int client_socket =
-        accept(server_socket, (sockaddr *)&client_address, &client_address_len);
-
-    if (client_socket < 0) {
-      std::cerr << "Failed to accept client connection." << std::endl;
-      continue;
-    }
-
+void acceptConnections(int serverSocket)
+{
+    while (serverRunning)
     {
-      std::lock_guard<std::mutex> lock(clients_mutex);
-      clients[client_socket] = client_address;
-    }
+        sockaddr_in clientAddress;
+        socklen_t clientAddressLen = sizeof(clientAddress);
+        int clientSocket =
+            accept(serverSocket, (sockaddr *)&clientAddress, &clientAddressLen);
 
-    std::thread client_thread(handle_client, client_socket);
-    client_thread.detach();
-  }
+        if (clientSocket < 0)
+        {
+            if (!serverRunning)
+            {
+                break; // Break if the server is no longer running
+            }
+            std::cerr << "Failed to accept client connection." << std::endl;
+            continue;
+        }
+
+        {
+            std::lock_guard<std::mutex> lock(clientsMutex);
+            clients[clientSocket] = clientAddress;
+        }
+
+        std::thread clientThread(handleClient, clientSocket);
+        clientThread.detach();
+    }
 }
 
 /*----------------------------------Function-Definitions-End----------------------------------*/
 
-int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " <port>" << std::endl;
-    return 1;
-  }
-
-  int port = std::stoi(argv[1]);
-  int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_socket < 0) {
-    std::cerr << "Cannot open socket" << std::endl;
-    return 1;
-  }
-
-  sockaddr_in server_address{};
-  server_address.sin_family = AF_INET;
-  server_address.sin_addr.s_addr = INADDR_ANY;
-  server_address.sin_port = htons(port);
-
-  if (bind(server_socket, (sockaddr *)&server_address, sizeof(server_address)) <
-      0) {
-    std::cerr << "Cannot bind socket to port " << port << std::endl;
-    return 1;
-  }
-
-  if (listen(server_socket, 5) < 0) {
-    std::cerr << "Cannot listen on port" << std::endl;
-    return 1;
-  }
-
-  std::cout << "Server is listening on port " << port << std::endl;
-  std::thread accept_thread(accept_connections, server_socket);
-  command_loop();
-
-  // Scope created to automatically destry lock_guard
-  {
-    std::lock_guard<std::mutex> lock(clients_mutex);
-    for (const auto &client : clients) {
-      close(client.first);
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <port>" << std::endl;
+        return 1;
     }
-    clients.clear();
-  }
 
-  close(server_socket);
-  accept_thread.join();
-  return 0;
+    int port = std::stoi(argv[1]);
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket < 0)
+    {
+        std::cerr << "Cannot open socket" << std::endl;
+        return 1;
+    }
+
+    sockaddr_in serverAddress{};
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(port);
+
+    if (bind(serverSocket, (sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+    {
+        std::cerr << "Cannot bind socket to port " << port << std::endl;
+        return 1;
+    }
+
+    if (listen(serverSocket, 5) < 0)
+    {
+        std::cerr << "Cannot listen on port" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Server is listening on port " << port << std::endl;
+    std::thread acceptThread(acceptConnections, serverSocket);
+    commandLoop();
+
+    shutdown(serverSocket, SHUT_RDWR); // Stop accepting new connections
+
+    // Scope created to automatically destroy lock_guard
+    {
+        std::lock_guard<std::mutex> lock(clientsMutex);
+        for (const auto &client : clients)
+        {
+            close(client.first);
+        }
+        clients.clear();
+    }
+
+    close(serverSocket);
+    acceptThread.join();
+    return 0;
 }
